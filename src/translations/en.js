ac48de2af5d63f60f18283a47bf2b124e6cdec2a
Cameron Chardukian <camchardukian@gmail.com>
2021-08-02 21:35:34 +0700

    feature/localization

---
+++ src/translations/en.js
@@ -0,0 +1,10 @@
+const en = {
+  todoList: {
+    title: "Todo List",
+    addTodo: "Add Todo",
+    toggleChecked: "Toggle All Checked",
+    toggleUnchecked: "Toggle All Unchecked"
+  }
+};
+
+export default en;
